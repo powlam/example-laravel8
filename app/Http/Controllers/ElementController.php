17776f57f6e6ead30dd7f12effe5c200323ef8e8
Paul Albandoz <palbandoz@gmail.com>
2021-09-20 10:02:55 +0200

    Element resource controller: only index

---
+++ app/Http/Controllers/ElementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Element;
+
+class ElementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $elements = Element::all();
+        return view('element.index', compact('elements'));
+    }
+
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  \App\Models\Element  $element
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(Element $element)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Models\Element  $element
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit(Element $element)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\Models\Element  $element
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, Element $element)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  \App\Models\Element  $element
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(Element $element)
+    // {
+    //     //
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppController;
+use App\Http\Controllers\ElementController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [AppController::class, 'welcome']);
+
+Route::resource('element', ElementController::class)->only([
+    'index',
+]);
